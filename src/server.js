0d916c72dc00341389e477e30f990a9960ee4b6b
Procech20 <mupinoe@gmail.com>
2021-02-19 14:54:31 +0200

    first app setup

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+
+//Body parser
+app.use(express.json());
+
+
+
+const PORT = process.env.PORT || 5000;
+
+const port = app.listen(PORT, console.log(`Server running in "${process.env.NODE_ENV}" mode on port ${PORT}...`));
